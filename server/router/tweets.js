a313f407435ca19a65e8d6776f2ea1ec69774cfd
ymb3264 <a01028371042@gmail.com>
2021-12-17 17:14:19 +0900

    validate

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,22 +1,36 @@
-import express from 'express';
-import 'express-async-errors';
-import * as tweetController from '../controller/tweet.js';
+import express from "express";
+import "express-async-errors";
+import { body } from "express-validator";
+import * as tweetController from "../controller/tweet.js";
+import { validate } from "../middleware/validator.js";
+
+// validation
+// sanitization
+
 const router = express.Router();
+
+const validateTweet = [
+  body("text")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("text should be at least 3 characters"),
+  validate,
+];
 
 // GET /tweets
 // GET /tweets?username=:username
-router.get('/', tweetController.getTweets);
+router.get("/", tweetController.getTweets);
 
 // GET /tweets/:id
-router.get('/:id', tweetController.getTweet);
+router.get("/:id", tweetController.getTweet);
 
 // POST /tweeets
-router.post('/', tweetController.createTweet);
+router.post("/", validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put("/:id", validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets/:id
-router.delete('/:id', tweetController.deleteTweet);
+router.delete("/:id", tweetController.deleteTweet);
 
 export default router;
