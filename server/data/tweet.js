427feeea196ed6d8660d5d95705a9fcd10846272
ymb3264 <a01028371042@gmail.com>
2021-12-24 15:47:31 +0900

    auth

--- server/data/tweet.js
+++ server/data/tweet.js
@@ -1,18 +1,18 @@
 let tweets = [
   {
-    id: '1',
-    text: '드림코더분들 화이팅!',
+    id: "1",
+    text: "드림코더분들 화이팅!",
     createdAt: Date.now().toString(),
-    name: 'Bob',
-    username: 'bob',
-    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+    name: "Bob",
+    username: "bob",
+    url: "https://upload.wikimedia.org/wikipedia/ko/thumb/a/ae/Chelsea_FC_Logo.svg/1200px-Chelsea_FC_Logo.svg.png",
   },
   {
-    id: '2',
-    text: '안뇽!',
+    id: "2",
+    text: "안뇽!",
     createdAt: Date.now().toString(),
-    name: 'Ellie',
-    username: 'ellie',
+    name: "Ellie",
+    username: "ellie",
   },
 ];
 
